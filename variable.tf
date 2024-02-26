85a42f8b90de081cff8622dd867333962a08ba98
k-vamc <152714189+k-vamc@users.noreply.github.com>
2024-02-26 17:15:44 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,37 +8,44 @@ variable "vpc_cidr" {
 
 #cidr block for 1st subnet
 
-variable "subnet1_cidr" {
+variable "sub-1_cidr" {
   default = "10.0.1.0/24"
 }
 
 #cidr block for 2nd subnet
 
-variable "subnet2_cidr" {
+variable "sub-2_cidr" {
   default = "10.0.2.0/24"
 }
 
 #cidr block for 3rd subnet
 
-variable "subnet3_cidr" {
+variable "sub-3_cidr" {
   default = "10.0.3.0/24"
 }
 
 #cidr block for 4th subnet
 
-variable "subnet4_cidr" {
+variable "sub-4_cidr" {
   default = "10.0.4.0/24"
 }
 
 #cidr block for 5th subnet
 
-variable "subnet5_cidr" {
+variable "sub-5_cidr" {
   default = "10.0.5.0/24"
 }
 
 #cidr block for 6th subnet
 
-variable "subnet6_cidr" {
+variable "sub-6_cidr" {
   default = "10.0.6.0/24"
 }
+#vpc
+resource "aws_vpc" "demo-vpc" {
+  cidr_block       = var.10.0.0.0/16
 
+  tags = {
+    Name = "demo-vpc"
+  }
+}
